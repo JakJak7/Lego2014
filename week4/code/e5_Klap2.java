beaba96751e6adfb551280704b0795809d1537a0
knakke3 <knakke3@cs.au.dk>
2014-02-20 15:38:00 +0100

    e5 klap og klap2 (test for nedagående sekvens for at passe bedre til et klap
    Former-commit-id: f8562805d89df4823a20f057b02608279714c9e3

---
+++ week4/code/e5_Klap2.java
@@ -0,0 +1,51 @@
+import lejos.nxt.*;
+/**
+ * The locomotions of a  LEGO 9797 car is controlled by
+ * sound detected through a microphone on port 1. 
+ * 
+ * @author  Ole Caprani
+ * @version 23.08.07
+ */
+public class e5_Klap {
+
+    private static SoundSensor sound = new SoundSensor(SensorPort.S1);
+	
+    public static void main(String [] args) throws Exception
+    {
+	Button.ESCAPE.addButtonListener(new ButtonListener() {
+		public void buttonPressed(Button b) {
+		    LCD.drawString("ENTER pressed", 0, 0);
+		    System.exit(1);
+		}
+
+		public void buttonReleased(Button b) {
+		    LCD.clear();
+		}
+	    });
+	   	   
+        int soundLevel;
+	int low = 30;
+	int high = 70;
+        while (true) {
+	    soundLevel = sound.readValue();
+	    LCD.drawInt(soundLevel,4,10,0); 
+	    if (soundLevel < low) {
+		Thread.sleep(30);
+		soundLevel = sound.readValue();
+		if (soundLevel > high) {
+		    Thread.sleep(100-30);
+		    soundLevel = sound.readValue();
+		    if (soundLevel > low && soundLevel <40) {
+			Thread.sleep(150);
+			soundLevel = sound.readValue();
+			if (soundLevel < low) {
+			    LCD.drawString("KLAP DETECTED!!", 0, 0);
+			    Thread.sleep(1000);
+			    LCD.clear();
+			}
+		    }
+		}
+	    }
+	}
+    }
+}
