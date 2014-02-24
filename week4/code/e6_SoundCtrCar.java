bfa3bfb98395f6c622558e3d3eb749a944c1ea3e
knakke3 <knakke3@cs.au.dk>
2014-02-24 15:18:13 +0100

    lort
    Former-commit-id: 3c47d4152b184750377cf212b739c5a0bffa60ad

---
+++ week4/code/e6_SoundCtrCar.java
@@ -0,0 +1,42 @@
+import lejos.nxt.*;
+/**
+ * The locomotions of a  LEGO 9797 car is controlled by
+ * sound detected through a microphone on port 1. 
+ * 
+ * @author  Ole Caprani
+ * @version 23.08.07
+ */
+public class e6_SoundCtrCar 
+{
+    private static SoundSensor sound1 = new SoundSensor(SensorPort.S1);
+    private static SoundSensor sound2 = new SoundSensor(SensorPort.S2);
+	
+    public static void main(String [] args) throws Exception
+    {
+	Button.ESCAPE.addButtonListener(new ButtonListener() {
+		public void buttonPressed(Button b) {
+		    LCD.drawString("ESCAPE pressed", 0, 0);
+		    System.exit(1);
+		}
+		
+		public void buttonReleased(Button b) {
+		    LCD.clear();
+		}
+	    });
+
+        LCD.drawString("Party!!: ",0,0);
+        LCD.refresh();
+	int soundLevel1,soundLevel2;
+        while (! Button.ESCAPE.isDown()) {
+	    soundLevel1 = sound1.readValue();
+	    soundLevel2 = sound2.readValue();
+	    LCD.drawInt(soundLevel1,4,10,0); 
+	    LCD.drawInt(soundLevel2,4,10,1); 
+            Car.forward(soundLevel1*3, soundLevel2*3);
+       }
+       Car.stop();
+       LCD.clear();
+       LCD.drawString("Program stopped", 0, 0);
+       Thread.sleep(2000); 
+   }
+}
