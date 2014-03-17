b1d9ef1e47f6474e0eaf437a8c3872629a28c76a
knakke3 <knakke3@cs.au.dk>
2014-03-17 13:04:03 +0100

    Vehicle2
    Former-commit-id: f81122fe690b98c51ccdabfb99a9c86ff4a7a145

--- week7/code/Vehicle2.java
+++ week7/code/Vehicle2.java
@@ -2,31 +2,44 @@ import lejos.nxt.*;
 import java.lang.Math;
 
 public class Vehicle2 {
-    private int behavior;
-    private int sValue() {
-	int value = LightSensor(SensorPort.S1,true);
-        LCD.drawString("sValue: ",0,0);
-	LCD.drawInt(value,4,10,0); 
-	return value;
+    private int lValue(int i) {
+	int value = (i == 1)?SensorPort.S1:SensorPort.S2;
+        LCD.drawString("lValue: ",0,i-1);
+	LCD.drawInt(value,4,10,i-1); 
+	return map(value);
     }
 
-    private void mPower(int power) {
-	int d1 = (power > 0)?1:2;
-	power = (power < 0)?-power:power;
-        LCD.drawString("mPower: ",0,1);
-	LCD.drawInt((int) (Math.pow(-1,d1+1)*power),4,10,1); 
+    private void mPower(int p1, int p2) {//-100 -> 100
+	int d1 = (p1 > 0)?1:2;
+	int d2 = (p2 > 0)?1:2;
+	if (p1 < 0) p1 = -p1;
+	if (p2 < 0) p2 = -p2;
+	
+        LCD.drawString("mPower1: ",0,2);
+	LCD.drawInt((int) (Math.pow(-1,d1+1)*p1),4,10,2); 
+	LCD.drawString("mPower2: ",0,3);
+	LCD.drawInt((int) (Math.pow(-1,d2+1)*p2),4,10,3); 
 
-	if (power < 50) // anti hyl
-	    d1 = 4;
-	MotorPort.B.controlMotor(power,d1);
-	MotorPort.C.controlMotor(power,d1);
+	// anti hyl!
+	if (p1 < 50) d1 = 4;
+	if (p2 < 50) d2 = 4;
+
+	MotorPort.B.controlMotor(p1,d1);
+	MotorPort.C.controlMotor(p2,d2);
     }
 
-    public Vehicle2(int behavior) throws Exception {
-	this.behavior = behavior;
-	SensorPort.S1.setTypeAndMode(SensorConstants.TYPE_SOUND_DB,SensorConstants.MODE_RAW);
+    private int map(int raw) {
+	// Hvordan er mapping på light- og sonic-sensor
+	return (int) (100-raw/1023.0*100);
+    }
+
+    public Vehicle2() throws Exception {
+	SensorPort.S1.setTypeAndMode(SensorConstants.TYPE_LIGHT_INACTIVE,SensorConstants.MODE_RAW);
 	Thread.sleep(120); //Sensor value falling
-	run();
+
+	while (!Button.ESCAPE.isDown()) {
+	    mPower(lValue(1),lValue(2));
+	}
 	LCD.clear();
     }
 
