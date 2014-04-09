0cb947251c08b293399decec44a43e3b3f064a17
knakke3 <knakke3@cs.au.dk>
2014-04-09 15:13:57 +0200

    Motor problemet er gjort mere simpelt at vise i code33.java
    Former-commit-id: 6394d92b9cc95b1e86dae27adea5747d9189771f

---
+++ week9/code/Code33.java
@@ -0,0 +1,33 @@
+import lejos.nxt.*;
+import lejos.robotics.Color;
+
+public class Code33 {
+
+    public Code33() {
+        while (true) {
+	    if (Button.RIGHT.isDown()) {
+		Motor.A.forward();
+		Motor.B.forward();
+	    }
+	    else {
+		Motor.A.backward();
+		Motor.B.backward();
+	    }
+	    while (Button.LEFT.isDown()){
+		LCD.drawInt(-666,4,10,0);
+		Motor.A.stop();
+		Motor.B.stop();
+	    }
+	}
+
+    }
+
+    public static void main(String[] args) {
+	Button.ESCAPE.addButtonListener(new ButtonListener() {
+		public void buttonPressed(Button b) {System.exit(1);}
+		public void buttonReleased(Button b) {} 
+	    });
+	
+	new Code33();
+    }
+}
