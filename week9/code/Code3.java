d3b15aa2350858d5f2e2ed7e8a0b8e665d8825ea
knakke3 <knakke3@cs.au.dk>
2014-04-10 11:29:08 +0200

    Motor virker nu,.. og log er opdateret
    Former-commit-id: e276a5f1e98cd78621d9ba94cdd1aeaf7dca1e7b

--- week9/code/Code3.java
+++ week9/code/Code3.java
@@ -1,15 +1,23 @@
 import lejos.nxt.*;
 import lejos.robotics.Color;
+import lejos.robotics.*;
+import lejos.nxt.addon.*;
 
 public class Code3 {
     private final LightSensor l1 = new LightSensor(SensorPort.S1,false);
     private final LightSensor l2 = new LightSensor(SensorPort.S2,false);
     private final ColorSensor cs = new ColorSensor(SensorPort.S3);
+    private final DCMotor ml = new RCXMotor(MotorPort.A);
+    private final DCMotor mr = new RCXMotor(MotorPort.B);
 
     private int getC() {return cs.getColor().getColor();}//1 = green, 7 = black, 6 = white?
+    private int getCL() {return cs.getNormalizedLightValue();}
     private int getL(int i){return ((i==1)?l1:l2).readValue();}
-            
+
     public Code3() {
+	ml.setPower(100);
+	mr.setPower(100);
+
 	cs.setFloodlight(Color.WHITE);
 	LCD.clear();
 	LCD.drawString("Light: ", 0, 0); 
@@ -19,22 +27,20 @@ public class Code3 {
         while (true) {
 	    LCD.drawInt(getL(1),4,10,0);
 	    int cc = getC();
-	    LCD.drawInt(cc ,4,10,1);
+	    LCD.drawInt(cc ,4,5,1);
+	    LCD.drawInt(getCL() ,4,10,1);
 	    LCD.drawInt(getL(2),4,10,2);
 	    
-	    if (cc == 0) {
-		Motor.A.forward();
-		Motor.B.forward();
+	    if (cc == 7) {
+		ml.forward();
+		mr.forward();
 	    }
-	    else {
-		Motor.A.backward();
-		Motor.B.backward();
-	    }
-	    while (Button.LEFT.isDown()){
-		LCD.drawInt(-666,4,10,0);
-		Motor.A.stop();
-		Motor.B.stop();
-	    }
+	    else
+	    if (cc == 6) {
+		ml.stop();
+		mr.stop();
+	    } 
+
 	}
 
     }
