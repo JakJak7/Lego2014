04ef2302b7469672caf94ff6fe32703886d50326
Knakke3 <knakke3@cs.au.dk>
2014-05-27 15:04:02 +0200

    grafer
    Former-commit-id: 56ae6b4a2445eff78d7d72d94d5a6f853e5254b0

--- exam/experiments/GridAccuracy.java
+++ exam/experiments/GridAccuracy.java
@@ -1,7 +1,8 @@
 import lejos.nxt.*;
+import java.io.*;
 
 public class GridAccuracy {
-    private LightSensor sensor = new LightSensor(SensorPort.S1,false);
+    private LightSensor sensor = new LightSensor(SensorPort.S1,true);
     private int green = -1;
     private int black = -1;
     private int white = -1;
@@ -14,9 +15,14 @@ public class GridAccuracy {
     public int rnv() {
 	return sensor.readNormalizedValue();
     }
+    private static File f;
+    private static FileOutputStream fos;
+
+    public static void writeS(String s) throws Exception {
+	for(int i=0; i<s.length(); i++)
+	    fos.write((byte)s.charAt(i));
+    }
     public void calibrate() {
-	controlMotor(MotorPort.B,-100);
-	controlMotor(MotorPort.C,-100);
 	// Green
 	green = rnv();
 	LCD.drawInt(green, 2, 7, 0);
@@ -28,17 +34,30 @@ public class GridAccuracy {
 	LCD.drawInt(white, 2, 7, 2);
 	
     }
-    public PID() {
+    public GridAccuracy() throws Exception {
 	LCD.drawString("green: ", 0, 0);
 	LCD.drawString("black: ", 0, 1);
 	LCD.drawString("white: ", 0, 2);
-	calibrate();
+	//calibrate();
+	controlMotor(MotorPort.B,0);
+	controlMotor(MotorPort.C,0);
+	controlMotor(MotorPort.B,-70);
+	controlMotor(MotorPort.C,-70);
+	f = new File("GridAccuracy.txt");
+	if (f.exists()) f.delete();
+	f.createNewFile();
+	fos = new FileOutputStream(f);
+	int samplenr = 0;
 	while (!Button.ESCAPE.isDown()) {
+	    writeS(samplenr+++"\t");
+	    writeS(rnv()+"\n");
+	    Thread.sleep(5);
 	    LCD.drawInt(green, 2, 7, 0);
 	    LCD.drawInt(black, 2, 7, 1);
 	    LCD.drawInt(white, 2, 7, 2);
 	    LCD.drawInt(rnv(), 2, 7, 5);
 	}
+	fos.close();
     }
     public static void main (String[] aArg) throws Exception {
 	new GridAccuracy();
