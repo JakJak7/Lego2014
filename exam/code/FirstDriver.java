43d11f50ee3c7428b0951c98ca679f950c9083e1
Knakke3 <knakke3@cs.au.dk>
2014-06-03 12:33:04 +0200

    Skrevet mere på FirstDriver, så den kan calibrer, dreje, følge linien
    Former-commit-id: 8c95632796b1795209b4f884841f534d4349a555

--- exam/code/FirstDriver.java
+++ exam/code/FirstDriver.java
@@ -4,16 +4,32 @@ import java.lang.Math;
 
 public class FirstDriver {
     private LightSensor sensor = new LightSensor(SensorPort.S1,true);
+    private NXTRegulatedMotor M3 = Motor.C;
+    private MotorPort MP1 = MotorPort.A, MP2 = MotorPort.B;
     private int green = -1;
     private int black = 1024;
     private int white = -1;
-    private int power = 600;
+    private int power = 70;
 
     public int light() {
 	return sensor.readNormalizedValue();
     }
+    public void grap() {
+	M3.rotateTo(-330);
+    }
+    public void release() {
+	M3.rotateTo(0);
+    }
+    public static void controlMotor(MotorPort m, int value) {
+	if      (value > 0 ) m.controlMotor(value,1);
+	else if (value < 0 ) m.controlMotor(-1*value,2);
+	else if (value == 0) m.controlMotor(value,3);
+    }
 
     public void calibrate() throws Exception {
+	//	controlMotor(MP1,power);
+	//controlMotor(MP2,power);
+
 	green = light();
 	LCD.drawInt(green, 2, 7, 0);
 	while (light() < green+40);
@@ -23,43 +39,46 @@ public class FirstDriver {
 	while (light() > white-40);
 	while (light() < white-20)
 	    black = (light() < black)?light():black;
+	LCD.drawInt(black, 2, 7, 1);
     }
     public void leftturn() {
 	float wheelSize = 5.5f;
 	float width = 19.6f;
 	double degrees = 360*((width*2 * Math.PI)/4) / (wheelSize * Math.PI);
-	degrees = degrees * 1.05f; //error correction because of inaccuracies
-	Motor.B.rotate((int)degrees/2,true);
-	Motor.C.rotate(-(int)degrees/2);
+	degrees = degrees * 1.1f; //error correction because of inaccuracies
+	Motor.A.rotate((int)degrees/2,true);
+	Motor.B.rotate(-(int)degrees/2);
     }
     public void followP() {
 	float Kp = 1f;
-	int Tp = 70;
+	int Tp = 60;
 	int offset = (int) (white+black)/2;
 	float Ki = 0.00000f;
 	while (! Button.ESCAPE.isDown()) {
 	    int error = light() - offset;
-	    int Turn = (int) (Kp*error)
-	    controlMotor(MotorPort.B,Tp+Turn);
-	    controlMotor(MotorPort.C,Tp-Turn);
+	    int Turn = (int) (Kp*error);
+	    controlMotor(MP1,(Tp+Turn));
+	    controlMotor(MP2,(Tp-Turn));
 	}
     }
     public FirstDriver() throws Exception {
-	Motor.B.setSpeed(power);
-	Motor.C.setSpeed(power);
+	Motor.A.setSpeed(600);// 2 RPM
+	Motor.B.setSpeed(600);
+	Motor.A.forward();
 	Motor.B.forward();
-	Motor.C.forward();
 
 	LCD.drawString("green: ", 0, 0);
 	LCD.drawString("black: ", 0, 1);
 	LCD.drawString("white: ", 0, 2);
 
+	//grap();
+	//release();
+
 	calibrate();
 	leftturn();
 	followP();
-	
     }
     public static void main (String[] aArg) throws Exception {
-	new GridAccuracy();
+	new FirstDriver();
     }
 }
